e78f29e3257073e063f4680254edbf932a3b83b2
winstantralf <ralf@winstantpay.com>
2018-08-05 18:32:14 +0700

    credentials

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# restpy
-The RESTful API to winstantKYC and Wallet
+# winstantpay-restpy
+
+![The WinstantPay Logo](http://www.winstantpay.com/assets/img/logo-winstantpay-L-notag-trans.png "The WinstantPay Logo")
+
+These are the Python examples for using the WorldKYC and WinstantPay REST APIs
+
+
+## Introduction
+
+WinstantPay allows anyone to trade or pay globally with any currency, including cryptos and other tokens anytime and from anywhere. Originating from a foreign currency exchange and trade finance background the WinstantPay core is utilized by WinstantPay to provide a solid means for ecosystem partners to develop mobile and electronic wallets on this platform. 
+
+### Install: ###
+Clone this repository then get started right away
+### Requirements: ###
+
+
+### Quick start
+
+```bash
+pip install zeep
+```
+>Please note that you need a pre-shared key to use the API. 
+>This key is sometimes still called a caller-id (We are transitioning that naming convention) to a more modern term
+>
+>To get the caller id, you need to complete your KYC (Know Your Client), which will result in >you have a user ID and password with WinstantPay. 
+>To complete the basic KYC, you need a working email and telephone number and head over to [WORLD-KYC](https://winstantpay.worldkyc.com/)
+
+>Once done, send us an email to <api@winstantpay.com> from the registered email and we ?will get in touch prompty (usually via SMS to your phone 24hours).
+>
+>Upon verification of that number we will provide your with the caller ID
+
+After you have all you credentials please follow the following steps (explained in section **Examples** below
+
+## Basic Flow of the API
+## To get a wallet 
+### KYC first
+Head right to the [Demo](https://demo.winstantpay.com/) platform and complete your KYC first.
+To complete your KYC you only need a working email address you can access. 
+After successful subscription to KYC your user credentials will be added to the system and you can login to the wallet. 
+After that, please send us an email so we can creaate a caller-id for you and your all set to go and use this API.
+
+
+## Wallet Demo
+
+https://demoewallet.winstantpay.com/
+
+## Support
+
+Support for the WinstantPay API is available through the WinstantPay API team. We will share the details about how to interact with our teams at the end of the KYC process.  Should you have pUiany issues before that you can send a twitter message to us to <api@winstantpay.com>
+
+## License
+
+WinstantPay API example scripts are released under the MIT license.
+
+
+
+
+
+
+
---
+++ run_me_first.py
@@ -0,0 +1,112 @@
+import config
+import requests
+import json
+from typing import Dict, List
+from pprint import pprint
+
+TOKEN:str = ''
+CLIENT_ID: int = 0
+
+
+def login() -> str:
+    global TOKEN
+    base_url: str = 'http://doc.worldkyc.com/client/auth/'
+    auth_record: json =  { "username": config.userLogin, "password": config.userPassword  }
+    result= requests.post(base_url, data=auth_record).json()
+    TOKEN = result["token"]
+
+    # result= requests.post(base_url, data=auth_record)
+    # print(json.dumps(auth_record, indent=2, sort_keys=True))
+    # print(result.text)
+    #
+    # exit(-1)
+
+    return TOKEN
+
+#
+# The readData fundtion reads the client data
+#
+def readData(client: int) -> json:
+
+    with open('./data/clients.json') as f:
+        data: json = json.load(f)
+
+    return data[client]
+
+#
+# The registerClient function
+#
+def registerClient(requirement_id: int, requirements: json ,client: json) -> json:
+
+    global CLIENT_ID
+    global TOKEN
+
+    base_url: str = 'http://doc.worldkyc.com/client/signup/'
+    APIkey:str  = config.APIKey
+    TOKEN =  login()
+    url: str = "{0}?service_caller_id={1}".format(base_url, APIkey)
+
+    if(requirement_id>0):
+        url = "{0}&requirement_id={1}".format(url, requirement_id)
+
+    if CLIENT_ID > 0:
+        url = "{0}&client_id={1}".format(url, CLIENT_ID)
+
+    print(url)
+
+    nc: json = {}
+
+    for elem in requirements["fields"]:
+        key:str =elem["name"]
+        value: str = client[key]
+        nc[key] = value
+
+    print(json.dumps(nc, indent=2, sort_keys=True))
+    trx_header = {'Authorization': 'token {}'.format(TOKEN)}
+    result = requests.post(url, headers = trx_header, data=nc).json()
+
+    print(json.dumps(result, indent=2, sort_keys=True))
+
+    if ('id' in result):
+        CLIENT_ID = result["id"]
+        print('Set Client Id {}'.format(CLIENT_ID))
+
+    return result
+
+#
+# The HandleStep fundtion
+#
+def handleStep(requirements_id: int, requirements: json ,url: str ) -> int:
+
+    client: json = readData(2)
+
+    newKYCClient:json = registerClient(requirements_id, requirements, client)
+
+    next_id = requirements["requirement_id"] # This field holds the next requirement
+
+    return next_id
+
+#
+# The nain
+#
+def main():
+    base_url: str = 'http://doc.worldkyc.com/client/schema/'
+    APIkey:str  = config.APIKey
+
+    TOKEN =  login()
+
+    url: str = base_url + '?service_caller_id=' + APIkey
+    result: json = requests.get(url).json()
+    next_id: int  =  handleStep(0, result,url)
+
+    while next_id != None:
+        url = base_url + '?service_caller_id=' + APIkey + '&requirement_id={}'.format(next_id)
+        result = requests.get(url).json()
+        next_id = handleStep(next_id,result,url)
+
+
+#
+# Invoke Main from here to solve accidential import
+#
+if __name__ == "__main__":
+    main()
